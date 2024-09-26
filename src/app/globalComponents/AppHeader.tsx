9148e7853f50a1d9e5389684e2e2f9ffbed7ae37
sutra-rudi <rudi@sutra.hr>
2024-09-26 14:06:17 +0200

    what is

--- src/app/globalComponents/AppHeader.tsx
+++ src/app/globalComponents/AppHeader.tsx
@@ -103,7 +103,7 @@ const AppHeader = () => {
   };
 
   return (
-    <nav className='bg-almost-white md:relative fixed z-20 w-full top-0'>
+    <nav className='bg-almost-white dark:bg-almost-black md:relative fixed z-20 w-full top-0'>
       {/* MOBILE */}
       <div
         className={`absolute z-40 w-full h-screen bg-almost-white dark:bg-almost-black inset-0 transition-all duration-300 flex items-center flex-col lg:pt-0 gap-12 justify-between py-24  ${
@@ -242,12 +242,12 @@ const AppHeader = () => {
           />
         </div>
         <div className='hidden w-full md:block md:w-auto'>
-          <ul className='flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+          <ul className='flex items-center font-medium p-4 md:p-0 mt-4 gap-4'>
             <li>
               <button
                 id='dropdownNavbarLink'
                 data-dropdown-toggle='dropdownNavbar'
-                className='flex items-center justify-between w-full py-2 px-3'
+                className='flex items-center justify-between w-full py-2 px-3 dark:text-almost-white'
                 onClick={toggleDropdown}
               >
                 Usluge
@@ -309,10 +309,7 @@ const AppHeader = () => {
             </li>
 
             <li>
-              <a
-                href={`/${currentLang}/contact`}
-                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-              >
+              <a href={`/${currentLang}/contact`} className='block py-2 px-3 dark:text-almost-white'>
                 Kontakt
               </a>
             </li>
